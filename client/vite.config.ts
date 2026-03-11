9dbea5469ec761a09e791bd6fef1c52747375244
Pramish Sharma <pramish.sharma@debugdream.com>
2026-03-11 16:22:10 +0545

    fix:frontend build fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,3 @@
-// import path from 'path'
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// })
-
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -22,10 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true,            // allow external access
-    port: 5173,
-    strictPort: true,
-    allowedHosts: 'all',   // allow any host (Localtunnel, Ngrok, etc.)
-  },
 })
+
